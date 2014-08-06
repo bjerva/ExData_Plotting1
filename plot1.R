a7fca9c2789b350cea327044f80e1bbcebaf60c6
Johannes Bjerva <johannesbjerva@gmail.com>
2014-08-06 16:26:22 +0200

    Plot 1 documentation

--- plot1.R
+++ plot1.R
@@ -1,8 +1,13 @@
+# Read data
 data <- read.table("household_power_consumption.txt", header=T, sep=';',
                    na.strings="?",
                    colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
+
+# Get global active power data
 x <- as.numeric(data$Global_active_power)
+
+# Plot to png
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 par(bg = "darkgray")
 hist(x, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
